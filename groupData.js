a3e6784cd7e7cc384517fc6e9f6c8595b5f64102
Tamara Elaye <tamaraelaye@gmail.com>
2024-09-16 10:40:57 +0100

    initialise project

---
+++ groupData.js
@@ -0,0 +1,142 @@
+export const groupData = [
+  {
+    "id": "1441231759320400",
+    "name": "Natural Cancer Cures",
+    "description": "No Description",
+    "url": "https://facebook.com/1441231759320400",
+    "photoUrl": "Missing Photo URL: 1",
+    "info": "Public · 16K members",
+    "members": "16K",
+    "privacy": "Public"
+  },
+  {
+    "id": "1511741012462773",
+    "name": "Beat Cancer Naturally.  Real People.  Real Stories",
+    "description": "No Description",
+    "url": "https://facebook.com/1511741012462773",
+    "photoUrl": "Missing Photo URL: 2",
+    "info": "Public · 17K members",
+    "members": "17K",
+    "privacy": "Public"
+  },
+  {
+    "id": "229643206632230",
+    "name": "Beating Cancer Today",
+    "description": "No Description",
+    "url": "https://facebook.com/229643206632230",
+    "photoUrl": "Missing Photo URL: 3",
+    "info": "Public · 9.9K members · 10 posts a day",
+    "members": "9.9K",
+    "privacy": "Public"
+  },
+  {
+    "id": "1415428072020958",
+    "name": "Natural Cancer Cures.",
+    "description": "No Description",
+    "url": "https://facebook.com/1415428072020958",
+    "photoUrl": "Missing Photo URL: 4",
+    "info": "Public · 2.7K members",
+    "members": "2.7K",
+    "privacy": "Public"
+  },
+  {
+    "id": "779519632116593",
+    "name": "CANCER natural cures - healthy products",
+    "description": "No Description",
+    "url": "https://facebook.com/779519632116593",
+    "photoUrl": "Missing Photo URL: 5",
+    "info": "Public · 4.6K members",
+    "members": "4.6K",
+    "privacy": "Public"
+  },
+  {
+    "id": "186621013035",
+    "name": "CANNABIS CURES CANCERS!",
+    "description": "No Description",
+    "url": "https://facebook.com/186621013035",
+    "photoUrl": "Missing Photo URL: 6",
+    "info": "Public · 26K members",
+    "members": "26K",
+    "privacy": "Public"
+  },
+  {
+    "id": "203189443179739",
+    "name": "Cancer Preventions & Natural Cures*",
+    "description": "No Description",
+    "url": "https://facebook.com/203189443179739",
+    "photoUrl": "Missing Photo URL: 7",
+    "info": "Public · 2.6K members",
+    "members": "2.6K",
+    "privacy": "Public"
+  },
+  {
+    "id": "449080739321748",
+    "name": "Cancer without Chemo",
+    "description": "No Description",
+    "url": "https://facebook.com/449080739321748",
+    "photoUrl": "Missing Photo URL: 8",
+    "info": "Public · 10K members",
+    "members": "10K",
+    "privacy": "Public"
+  },
+  {
+    "id": "258485520939385",
+    "name": "CANCER CURE",
+    "description": "No Description",
+    "url": "https://facebook.com/258485520939385",
+    "photoUrl": "Missing Photo URL: 9",
+    "info": "Public · 33K members · 2 posts a day",
+    "members": "33K",
+    "privacy": "Public"
+  },
+  {
+    "id": "197860723559725",
+    "name": "Natural cures and remedies",
+    "description": "No Description",
+    "url": "https://facebook.com/197860723559725",
+    "photoUrl": "Missing Photo URL: 10",
+    "info": "Public · 25K members",
+    "members": "25K",
+    "privacy": "Public"
+  },
+  {
+    "id": "1836373746690709",
+    "name": "Prostate Cancer cure with Natural Remedies. Alternative methods of Healing.",
+    "description": "No Description",
+    "url": "https://facebook.com/1836373746690709",
+    "photoUrl": "Missing Photo URL: 11",
+    "info": "Public · 4K members",
+    "members": "4K",
+    "privacy": "Public"
+  },
+  {
+    "id": "1752645665108602",
+    "name": "Fenbendazole, DCA - Cancer Support Community",
+    "description": "No Description",
+    "url": "https://facebook.com/1752645665108602",
+    "photoUrl": "Missing Photo URL: 12",
+    "info": "Public · 65K members · 2 posts a day",
+    "members": "65K",
+    "privacy": "Public"
+  },
+  {
+    "id": "1016320645064179",
+    "name": "Cancer Herbal Treatment",
+    "description": "No Description",
+    "url": "https://facebook.com/1016320645064179",
+    "photoUrl": "Missing Photo URL: 13",
+    "info": "Public · 13K members",
+    "members": "13K",
+    "privacy": "Public"
+  },
+  {
+    "id": "697530667297243",
+    "name": "CANCER NATURAL DIETS",
+    "description": "No Description",
+    "url": "https://facebook.com/697530667297243",
+    "photoUrl": "Missing Photo URL: 14",
+    "info": "Public · 1K members",
+    "members": "1K",
+    "privacy": "Public"
+  }
+]
---
+++ postclean.js
@@ -0,0 +1,3 @@
+//now that the top post containinin gthe keywords has been feteched, 
+//run each post 
+//via the scraper false information to get its truthy status
